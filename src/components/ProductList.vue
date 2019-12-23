5035c3beeb546bad9a7d473a2ff2f53bb24ad3e0
Thapa <ujjwalthapa2307@gmail.com>
2019-12-23 18:11:53 +0900

    Vuex_modules and namespaced vuex modules

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,51 +1,57 @@
 <template>
-  <div>
-    <h1> ProductList </h1>
-    <img
-    v-if="loading"
-    src="https://i.imgur.com/JfPpwOA.gif"
-    >
-    <ul v-else>
-      <li v-for="product in products">
-        {{product.title}} - {{product.price | currency}}
-        <button
-        :disabled="!productIsInStock(product) "
-        @click="addProductToCart(product)"> Add to Cart </button>
-      </li>
-    </ul>
-  </div>
+    <div>
+      <h1>Product List</h1>
+      <img
+        v-if="loading"
+        src="https://i.imgur.com/JfPpwOA.gif"
+      >
+      <ul v-else>
+        <li v-for="product in products">
+          {{product.title}} - {{product.price | currency}} - {{product.inventory}}
+          <button
+            :disabled="!productIsInStock(product)"
+            @click="addProductToCart(product)"
+          >Add to cart</button>
+        </li>
+      </ul>
+    </div>
 </template>
 
 <script>
-import {mapState, mapGetters, mapActions} from 'vuex'
-export default {
-  data () {
-    return {
-      loading: false
-    }
-  },
-  computed: {
-    ...mapState({
-      products: state => state.products
-    }),
+  import {mapState, mapGetters, mapActions} from 'vuex'
+  export default {
+    data () {
+      return {
+        loading: false,
+        productIndex: 1
+      }
+    },
 
-    ...mapGetters({
-      productIsInStock: 'productIsInStock'
-    })
-  },
-  methods: {
-    ...mapActions({
-      fetchProducts: 'fetchProducts',
-      addProductToCart: 'addProductToCart'
-    }),
-  },
-  created () {
-    this.loading = true
-    this.fetchProducts()
-      .then(() => this.loading = false )
+    computed: {
+      ...mapState({
+        products: state => state.products.items
+      }),
+
+      ...mapGetters('products', {
+        productIsInStock: 'productIsInStock'
+      })
+    },
+
+    methods: {
+      ...mapActions({
+        fetchProducts: 'products/fetchProducts',
+        addProductToCart: 'cart/addProductToCart'
+      })
+    },
+
+    created () {
+      this.loading = true
+      this.fetchProducts()
+        .then(() => this.loading = false)
+    }
   }
-}
 </script>
 
-<style lang="css" scoped>
+<style scoped>
+
 </style>
