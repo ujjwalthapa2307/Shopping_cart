01d0cfdebac2195c5bd05b7541abfecaefcc21b5
Thapa <ujjwalthapa2307@gmail.com>
2019-12-23 11:40:02 +0900

    Cart items and total

--- src/store/index.js
+++ src/store/index.js
@@ -17,6 +17,19 @@ export default new Vuex.Store({
       // = computed properties
       availableProducts (state, getters) {
         return state.products.filter( product => product.inventory > 0)
+      },
+      cartProducts (state) {
+        return state.cart.map(cartItem => {
+          const product = state.products.find(product => product.id === cartItem.id)
+          return {
+            title: product.title,
+            price: product.price,
+            quantity: cartItem.quantity
+          }
+        })
+      },
+      cartTotal (state, getters) {
+        return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
       }
   },
 
